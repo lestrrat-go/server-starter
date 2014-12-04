ed36083c424b1bb4d98a43687e63878c078ffb46
Daisuke Maki <lestrrat+github@gmail.com>
2014-12-04 21:14:44 +0900

    More parameter stuff

--- cmd/start_server/start_server.go
+++ cmd/start_server/start_server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/jessevdk/go-flags"
@@ -13,20 +14,20 @@ import (
 type options struct {
 	OptArgs         []string
 	OptCommand      string
-	OptDir          string
-	OptInterval     time.Duration // Time between checks for liveness
-	OptPidFile      string
-	OptPorts        []int     `long:"port" description:"TCP port to listen to (if omitted, will not bind to any ports)"`
-	OptPaths        []string  // Paths (UNIX domain socket) to bind to
-	OptSignalOnHUP  os.Signal // Signal to send when HUP is received
-	OptSignalOnTERM os.Signal // Signal to send when TERM is received
+	OptDir          string    `long:"dir" arg:"path" description:"working directory, start_server do chdir to before exec (optional)"`
+	OptInterval     int       `long:"interval" arg:"seconds" description:"minimum interval (in seconds) to respawn the server program (default: 1)"`
+	OptPidFile      string    `long:"pid-file" arg:"filename" description:"if set, writes the process id of the start_server process to the file"`
+	OptPorts        []int     `long:"port" arg:"(port|host:port)" description:"TCP port to listen to (if omitted, will not bind to any ports)"`
+	OptPaths        []string  `long:"path" arg:"path" description:"path at where to listen using unix socket (optional)"`
+	OptSignalOnHUP  os.Signal `long:"signal-on-hup" arg:"Signal" description:"name of the signal to be sent to the server process when start_server\nreceives a SIGHUP (default: SIGTERM). If you use this option, be sure to\nalso use '--signal-on-term' below."`
+	OptSignalOnTERM os.Signal `long:"signal-on-term" arg:"Signal" description:"name of the signal to be sent to the server process when start_server\nreceives a SIGTERM (default: SIGTERM)"`
 	OptStatusFile   string
 }
 
 func (o options) Args() []string          { return o.OptArgs }
 func (o options) Command() string         { return o.OptCommand }
 func (o options) Dir() string             { return o.OptDir }
-func (o options) Interval() time.Duration { return o.OptInterval }
+func (o options) Interval() time.Duration { return time.Duration(o.OptInterval) * time.Second }
 func (o options) PidFile() string         { return o.OptPidFile }
 func (o options) Ports() []int            { return o.OptPorts }
 func (o options) Paths() []string         { return o.OptPaths }
@@ -39,7 +40,11 @@ func showHelp() {
 	// because I wanted to tweak the format just a bit... but
 	// there wasn't an easy way to do so
 	os.Stderr.WriteString(`
-Usage: start_server [options] cmd [args...]
+Usage:
+      start_server [options] -- server-prog server-arg1 server-arg2 ...
+
+      # start Plack using Starlet listening at TCP port 8000
+      start_server --port=8000 -- plackup -s Starlet --max-workers=100 index.psgi
 
 Options:
 `)
@@ -50,19 +55,17 @@ Options:
 		if tag == "" {
 			continue
 		}
-		var o string
-		if s := tag.Get("short"); s != "" {
-			o = fmt.Sprintf("-%s, --%s", tag.Get("short"), tag.Get("long"))
-		} else {
-			o = fmt.Sprintf("--%s", tag.Get("long"))
+		if s := tag.Get("long"); s != "" {
+			fmt.Fprintf(os.Stderr, "  --%s", s)
+			if a := tag.Get("arg"); a != "" {
+				fmt.Fprintf(os.Stderr, "=%s", a)
+			}
+			fmt.Fprintf(os.Stderr, ":\n")
 		}
-
-		fmt.Fprintf(
-			os.Stderr,
-			"  %-21s %s\n",
-			o,
-			tag.Get("description"),
-		)
+		for _, l := range strings.Split(tag.Get("description"), "\n") {
+			fmt.Fprintf(os.Stderr, "    %s\n", l)
+		}
+		fmt.Fprintf(os.Stderr, "\n")
 	}
 }
 
@@ -81,7 +84,7 @@ func _main() (st int) {
 		return
 	}
 	if opts.OptInterval < 0 {
-		opts.OptInterval = time.Second
+		opts.OptInterval = 1
 	}
 	opts.OptCommand = args[0]
 	if len(args) > 1 {
