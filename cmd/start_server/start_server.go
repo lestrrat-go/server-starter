f3afd6ab9e2cdb8867ca1f3f09cd0698fd02827c
Daisuke Maki <lestrrat+github@gmail.com>
2014-12-09 09:54:02 +0900

    Add more options, even if they are not implemented yet

--- cmd/start_server/start_server.go
+++ cmd/start_server/start_server.go
@@ -16,12 +16,19 @@ type options struct {
 	OptCommand      string
 	OptDir          string   `long:"dir" arg:"path" description:"working directory, start_server do chdir to before exec (optional)"`
 	OptInterval     int      `long:"interval" arg:"seconds" description:"minimum interval (in seconds) to respawn the server program (default: 1)"`
-	OptPidFile      string   `long:"pid-file" arg:"filename" description:"if set, writes the process id of the start_server process to the file"`
 	OptPorts        []string `long:"port" arg:"(port|host:port)" description:"TCP port to listen to (if omitted, will not bind to any ports)"`
 	OptPaths        []string `long:"path" arg:"path" description:"path at where to listen using unix socket (optional)"`
 	OptSignalOnHUP  string   `long:"signal-on-hup" arg:"Signal" description:"name of the signal to be sent to the server process when start_server\nreceives a SIGHUP (default: SIGTERM). If you use this option, be sure to\nalso use '--signal-on-term' below."`
 	OptSignalOnTERM string   `long:"signal-on-term" arg:"Signal" description:"name of the signal to be sent to the server process when start_server\nreceives a SIGTERM (default: SIGTERM)"`
-	OptStatusFile   string
+	OptPidFile      string   `long:"pid-file" arg:"filename" description:"if set, writes the process id of the start_server process to the file"`
+	OptStatusFile   string   `long:"status-file" arg:"filename" description:"if set, writes the status of the server process(es) to the file" note:"unimplemented"`
+	OptEnvdir string `long:"envdir" arg:"Envdir" description:"directory that contains environment variables to the server processes.\nIt is intended for use with \"envdir\" in \"daemontools\". This can be\noverwritten by environment variable \"ENVDIR\"." note:"unimplemented"`
+	OptEnableAutoRestart bool `long:"enable-auto-restart" description:"enables automatic restart by time. This can be overwritten by\nenvironment variable \"ENABLE_AUTO_RESTART\"." note:"unimplemented"`
+	OptAutoRestartInterval int `long:"auto-restart-interval" arg:"seconds" description:"automatic restart interval (default 360). It is used with\n\"--enable-auto-restart\" option. This can be overwritten by environment\nvariable \"AUTO_RESTART_INTERVAL\"." note:"unimplemented"`
+	OptKillOldDelay int `long:"kill-old-delay" arg:"seconds" description:"time to suspend to send a signal to the old worker. The default value is\n5 when \"--enable-auto-restart\" is set, 0 otherwise. This can be\noverwritten by environment variable \"KILL_OLD_DELAY\"." note:"unimplemented"`
+	OptRestart bool `long:"restart" description:"this is a wrapper command that reads the pid of the start_server process\nfrom --pid-file, sends SIGHUP to the process and waits until the\nserver(s) of the older generation(s) die by monitoring the contents of\nthe --status-file" note:"unimplemented"`
+	OptHelp bool `long:"help" description:"prints this help"`
+	OptVersion bool `long:"version" description:"printes the version number" note:"unimplemented"`
 }
 
 func (o options) Args() []string          { return o.OptArgs }
@@ -50,8 +57,35 @@ Options:
 `)
 
 	t := reflect.TypeOf(options{})
-	for i := 0; i < t.NumField(); i++ {
-		tag := t.Field(i).Tag
+
+	// This weird indexing stuff is done purely to keep ourselves
+	// compatible with the original start_server program
+	// (This is the order that the help is displayed in)
+	names := []string{
+		"OptPorts",
+		"OptPaths",
+		"OptDir",
+		"OptInterval",
+		"OptSignalOnHUP",
+		"OptSignalOnTERM",
+		"OptPidFile",
+		"OptStatusFile",
+		"OptEnvdir",
+		"OptEnableAutoRestart",
+		"OptAutoRestartInterval",
+		"OptKillOldDelay",
+		"OptRestart",
+		"OptHelp",
+		"OptVersion",
+	}
+
+	for _, name := range names {
+		f, ok := t.FieldByName(name)
+		if ! ok {
+			continue
+		}
+
+		tag := f.Tag
 		if tag == "" {
 			continue
 		}
@@ -59,6 +93,9 @@ Options:
 			fmt.Fprintf(os.Stderr, "  --%s", s)
 			if a := tag.Get("arg"); a != "" {
 				fmt.Fprintf(os.Stderr, "=%s", a)
+			}
+			if tag.Get("note") == "unimplemented" {
+				fmt.Fprintf(os.Stderr, " (UNIMPLEMENTED)")
 			}
 			fmt.Fprintf(os.Stderr, ":\n")
 		}
@@ -79,7 +116,7 @@ func _main() (st int) {
 	opts := &options{OptInterval: -1}
 	p := flags.NewParser(opts, flags.PrintErrors|flags.PassDoubleDash)
 	args, err := p.Parse()
-	if err != nil {
+	if err != nil || opts.OptHelp {
 		showHelp()
 		return
 	}
