4cda86fb5321c8c3cb5aae97fc47e106052550ca
lestrrat <49281+lestrrat@users.noreply.github.com>
2026-08-28 16:01:05 +0900

    regroup starter tests by production file (#44)

--- portspec_test.go
+++ portspec_test.go
@@ -1,26 +1,8 @@
 package starter
 
 import (
-	"net"
-	"os"
-	"path/filepath"
 	"testing"
 )
-
-func TestTeardownRemovesUnixSocket(t *testing.T) {
-	path := filepath.Join(t.TempDir(), "server.sock")
-	l, err := net.Listen("unix", path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	s := &Starter{listeners: []listener{{listener: l, spec: path}}}
-	if err := s.teardown(); err != nil {
-		t.Fatal(err)
-	}
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
-		t.Fatalf("unix socket path remains, stat error = %v", err)
-	}
-}
 
 func TestParsePortTarget(t *testing.T) {
 	for _, test := range []struct {
