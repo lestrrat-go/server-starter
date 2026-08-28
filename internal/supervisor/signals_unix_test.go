4a6e07527cfefce0fd679336a865404ac11be2a2
Daisuke Maki <49281+lestrrat@users.noreply.github.com>
2026-08-28 21:28:35 +0900

    fix SIGXFSZ canonical signal name

--- internal/supervisor/signals_unix_test.go
+++ internal/supervisor/signals_unix_test.go
@@ -9,6 +9,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestSIGXFSZCanonicalName(t *testing.T) {
+	require.Equal(t, "XFSZ", signame(syscall.SIGXFSZ))
+}
+
 func TestSigFromNameRecognizesXFSZ(t *testing.T) {
 	require.Equal(t, syscall.SIGXFSZ, SigFromName("XFSZ"))
 	require.Equal(t, syscall.SIGXFSZ, SigFromName("SIGXFSZ"))
