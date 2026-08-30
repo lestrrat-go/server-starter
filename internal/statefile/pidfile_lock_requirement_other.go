8f96dc4e31a930dd14133bea35e6d03198809998
Daisuke Maki <49281+lestrrat@users.noreply.github.com>
2026-08-30 10:22:30 +0900

    remove obsolete owner mode

--- internal/statefile/pidfile_lock_requirement_other.go
+++
@@ -1,7 +0,0 @@
-//go:build !linux && !windows
-
-package statefile
-
-func requireLockOwner() bool {
-	return false
-}
--- internal/statefile/pidfile_lock_requirement_windows.go
+++
@@ -1,7 +0,0 @@
-//go:build windows
-
-package statefile
-
-func requireLockOwner() bool {
-	return true
-}
